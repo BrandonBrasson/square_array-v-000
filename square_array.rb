ebe9389f25534ad02d317c2f1f557c42643414b8
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 16:23:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   squared_array = []
   array.each do |number|
-  number = [1,4,9]
+   number = number** 2
+  #number = [1,4,9]
     squared_array << number
   end
   squared_array
